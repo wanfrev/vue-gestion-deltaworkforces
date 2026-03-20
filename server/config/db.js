2bad04bad5b9ce40a34f968d9f7089706377bc53
wanfrev <wanfrevargas@gmail.com>
2026-03-19 22:27:06 -0400

    Bases

---
+++ server/config/db.js
@@ -0,0 +1,9 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  logging: false, // Para que no llene la consola de logs
+});
+
+module.exports = sequelize;
